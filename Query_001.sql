95c5cae05e2f13a3c82ea6c28a5f8631062c16bc
Nikita2540 <166852462+Nikita2540@users.noreply.github.com>
2025-01-12 22:42:25 +0530

    Update Query_001.sql

--- Query_001.sql
+++ Query_001.sql
@@ -6,10 +6,6 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-SHOW VARIABLES LIKE 'secure_file_priv'
-
-SELECT COUNT(patient_ID) FROM patients;
-
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\lifestyle.csv'
 INTO TABLE lifestyle
 FIELDS TERMINATED BY ','
