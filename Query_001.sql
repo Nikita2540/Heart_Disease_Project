6def662b1087c9ab9034935e2bfdd71c659d0e9b
Nikita <nsingh2540@gmail.com>
2025-01-12 22:39:23 +0530

    Version_2

--- Query_001.sql
+++ Query_001.sql
@@ -7,3 +7,26 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 SHOW VARIABLES LIKE 'secure_file_priv'
+
+SELECT COUNT(patient_ID) FROM patients;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\lifestyle.csv'
+INTO TABLE lifestyle
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\medical_history.csv'
+INTO TABLE medical_history
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\health_metrics.csv'
+INTO TABLE health_metrics
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
